8fea3a72b87342425e0d703859d085e46c6d6acc
chkheidzeatuka-cmyk <chkheidzeatuka@gmail.com>
2026-05-31 21:20:35 +0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,15 +1,20 @@
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/SiriusXFiles/Rayfield/main/source'))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
    Name = "My Delta Hub ⚡",
    LoadingTitle = "Delta Hub Loading...",
    LoadingSubtitle = "by chkheidzeatuka",
    ConfigurationSaving = {
-      Enabled = false
+      Enabled = false,
+      FolderName = "DeltaHub",
+      FileName = "HubConfig"
    },
    Discord = {
-      Enabled = false
-   }
+      Enabled = false,
+      Invite = "",
+      RememberJoins = false
+   },
+   KeySystem = false -- Tells Rayfield not to look for a password
 })
 
 local PlayerTab = Window:CreateTab("Player Mods", nil)
